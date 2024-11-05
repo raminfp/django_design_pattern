086c7a49b18283a2f5e16ae17c51ec9d703e7c26
Ramin Farajpour Cami <ramin.blackhat@gmail.com>
2024-11-05 09:22:19 +0330

    Update IndexView

--- django_design_pattern_app/api/v1/users/users.py
+++ django_design_pattern_app/api/v1/users/users.py
@@ -1,15 +1,13 @@
 from rest_framework import generics
 from rest_framework.schemas import AutoSchema
 from rest_framework.views import APIView
-from sentry_sdk import capture_exception
 from django_design_pattern_app.injector.base_injector import BaseInjector
 from django_design_pattern_app.middleware.exceptions import handle_exceptions
 from django_design_pattern_app.middleware.validate import validate_serializer
-from django_design_pattern_app.permissions import permissions
-from django_design_pattern_app.permissions.permissions import IsSuperUser
 from django_design_pattern_app.repositories.users_repo import UsersRepo
 from django_design_pattern_app.middleware.response import APIResponse
-from django_design_pattern_app.utils.validations import ValidateAndHandleErrors
+from django_design_pattern_app.permissions import permissions
+from django_design_pattern_app.permissions.permissions import IsSuperUser
 
 
 class BaseView(APIView, AutoSchema):
@@ -32,5 +30,4 @@ class IndexView(BaseView, generics.GenericAPIView):
         self.user_repo.minio_find()
         print("Starting... Call elasticsearch ")
         self.user_repo.elk_search()
-        # TODO : ...
         return APIResponse(data=True)
