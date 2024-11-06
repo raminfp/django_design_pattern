4d8f0258fcd7abe95eb667460f8f5b2b3adbf19d
Ramin Farajpour Cami <ramin.blackhat@gmail.com>
2024-11-06 12:08:08 +0330

    Update create_superuser.py

--- django_design_pattern_app/management/commands/create_superuser.py
+++ django_design_pattern_app/management/commands/create_superuser.py
@@ -2,19 +2,17 @@ from django.core.management.base import BaseCommand
 from django_design_pattern_app.models import Representations
 import os
 
-
 class Command(BaseCommand):
     help = 'Creates a superuser'
 
     def handle(self, *args, **options):
-        if Representations.objects.filter(username=os.getenv("SUPERUSER_USERNAME")).exists():
-            pass
+        if Users.objects.filter(username=os.getenv("SUPERUSER_USERNAME")).exists():
+            self.stdout.write(self.style.WARNING("Superuser already exists."))
         else:
-            Representations.objects.create_superuser(
+            Users.objects.create_superuser(
                 username=os.getenv("SUPERUSER_USERNAME"),
                 email=os.getenv("SUPERUSER_EMAIL"),
                 password=os.getenv("SUPERUSER_PASSWORD"),
-                agency_code=0,
                 is_superuser=True,
-                is_active=True
             )
+            self.stdout.write(self.style.SUCCESS("Superuser created successfully."))
